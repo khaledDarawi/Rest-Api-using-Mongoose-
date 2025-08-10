6c6805b58f53d91021d71e87f99a445da1f592a3
khaled waleed al darawi <khaleddarawi2005@gmail.com>
2025-08-10 12:50:03 +0300

    Update mongoose.js

--- mongoose.js
+++ mongoose.js
@@ -2,14 +2,10 @@ const express = require('express')
 const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 dotenv.config();
-
 const app = express()
 const port = 3000
 app.use(express.json())
-
-// i face a problem here so i asked chatGpt for solution and he gave me this solution (process.env.MONGO_URL = 'mongodb://localhost:27017/todosdb';) only
 process.env.MONGO_URL = 'mongodb://localhost:27017/todosdb';
-
 mongoose.connect('mongodb://localhost:27017/todosdb').then((result)=>{
     console.log("Connected")
 }).catch(err=>{
@@ -17,90 +13,51 @@ mongoose.connect('mongodb://localhost:27017/todosdb').then((result)=>{
 })
 
 const ToDoSchema = new mongoose.Schema({
-    id : Number,
-    task : String,
-    descreption : String,
+    task : String , 
+    descreption : String ,
 })
 
-const ToDo = mongoose.model('ToDo', ToDoSchema)
-
-let currentId = 1
+const ToDo = mongoose.model('ToDo',ToDoSchema)
 
 app.get('/todos', async (req, res) => {
-    try {
-        const todos = await ToDo.find()
-        res.json(todos)
-    } catch (err) {
-        console.error("ERROR", err)
-    }
-})
-
-app.post('/todos', async (req, res) => {
-    try {
-        const newToDo = req.body
-        newToDo.id = currentId++
-        await ToDo.create(newToDo)
-        console.log(newToDo)
-        res.json({ message: "Added new task" })
-    } catch (err) {
-        console.error("ERROR", err)
-    }
-})
-
-/*Not sure of this part but i hope it is right :)
-app.get('/todos/:id', async (req, res) => {
   try {
-      const taskId = parseInt(req.params.id)
-      const todo = await ToDo.findOne({ id: taskId })
-      if (!todo) {
-          return res.json({ message: "Task not found" });
-      }
-      res.json(todo);
-  } catch (err) {
-      res.json({ message: "Invalid ID" });
+    const todos = await ToDo.find();
+    res.json(todos);
+  } catch (error) {
+    console.error(error);
   }
 });
-*/
 
-app.put('/todos/:id', async (req, res) => {
-    try {
-        const todoID = parseInt(req.params.id);
-        const updatedTodo = req.body;
 
-        const todo = await ToDo.findOne({ id: todoID });
-        if (!todo) {
-            return res.json({ message: "The Task Id not found" });
-        }
-
-        if (updatedTodo.title) {
-            todo.title = updatedTodo.title;
-        }
-        if (updatedTodo.descreption) {
-            todo.descreption = updatedTodo.descreption;
-        }
-
-        await todo.save();
-        res.json(todo);
-    } catch (err) {
-        console.error("ERROR", err)
-    }
+app.post('/todos',async(req,res)=>{
+    const NewTask = new ToDo(req.body)
+    await NewTask.save()
+    res.json({message:"Added new task"})
 })
 
-app.delete('/todos/:id', async (req, res) => {
-    try {
-        const id = parseInt(req.params.id);
-        const result = await ToDo.deleteOne({ id: id });
-
-        if (result.deletedCount === 0) {
-            return res.json({ message: 'Task not found' });
-        }
-
-        res.json({ message: 'Task deleted' });
-    } catch (err) {
-        console.error("ERROR", err)
+app.get('/todos/:id', async (req, res) => {
+  try {
+    const task = await ToDo.findById(req.params.id);
+    if (!task) {
+      return res.json({ message: "Task not found" });
     }
+    res.json(task);
+  } catch (err) {
+    res.json({ message: "Invalid ID" });
+  }
+});
+
+app.put('/todos/:id',async(req , res)=>{
+    const updated = await ToDo.findByIdAndUpdate(req.params.id , req.body,{new:true})
+    res.json({message:"updated the task you want to do",updated})
 })
 
-app.listen(port, () => {
+app.delete('/todos/:id',async(req , res)=>{
+    await ToDo.findByIdAndDelete(req.params.id)
+  res.json({ message: 'we delete the id' })
+})
+
+app.listen(port , ()=>{
     console.log(`Server is running on http://localhost:${port}`);
 })
+
